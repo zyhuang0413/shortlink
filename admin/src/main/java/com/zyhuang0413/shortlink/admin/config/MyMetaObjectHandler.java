248891f7bda9d662cea75286834251a167e1d87b
zyhuang0413 <zyhuang0413@163.com>
2023-12-28 16:05:50 +0800

    feat(admin/config): 开发短链接后管系统通用持久层实体对象属性填充

---
+++ admin/src/main/java/com/zyhuang0413/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,29 @@
+package com.zyhuang0413.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author zyhuang
+ * Date: 2023/12/28
+ * Time: 15:25
+ * Description: MyBatis-Plus 自动填充功能
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
