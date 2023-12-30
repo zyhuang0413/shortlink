d0ef13dc2785c4c9b9f4a59cba3207a03a23bde7
zyhuang0413 <zyhuang0413@163.com>
2023-12-30 18:09:37 +0800

    feat(admin): 开发短链接后管系统删除短链接分组功能

--- admin/src/main/java/com/zyhuang0413/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/zyhuang0413/shortlink/admin/service/GroupService.java
@@ -36,4 +36,11 @@ public interface GroupService extends IService<GroupDO> {
      * @param requestParam 短链接修改分组请求参数
      */
     void updateGroup(ShortLinkGroupUpdateReqDTO requestParam);
+
+    /**
+     * 删除短链接分组
+     *
+     * @param gid 短链接分组标识
+     */
+    void deleteGroup(String gid);
 }
--- admin/src/main/java/com/zyhuang0413/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/zyhuang0413/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -63,6 +63,18 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         baseMapper.update(groupDO, updateWrapper);
     }
 
+    @Override
+    public void deleteGroup(String gid) {
+        LambdaUpdateWrapper<GroupDO> updateWrapper = Wrappers.lambdaUpdate(GroupDO.class)
+                .eq(GroupDO::getDelFlag, 0)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
+                .eq(GroupDO::getGid, gid);
+        GroupDO groupDO = new GroupDO();
+        groupDO.setDelFlag(1);
+        baseMapper.update(groupDO, updateWrapper);
+    }
+
+
     private boolean availableGroupId(String gid) {
         LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getGid, gid)
